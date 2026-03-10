b1f07653107809d927e93c32633698aab6b8a249
Veena Singh <vsingh08739@gmail.com>
2026-03-10 21:01:19 +0530

    Added new backend concepts

--- file.js
+++ file.js
@@ -1,10 +1,27 @@
 const fs = require("fs");
 
-//  // Sync...
+//  Sync...
 // fs.writeFileSync("./test.txt", "Hey There");
 
 // Async 
-fs.writeFile("./test.txt", "Hello Word As", (err) =>{})
+//fs.writeFile("./test.txt", "Hello Word As", (err) =>{})
 
+//const result = fs.readFileSync("./contact.txt", "utf-8")
+//console.log(result);
 
+//fs.readFile("./contacts.txt", "utf-8", (err, result) => {
+   // if(err) {
+    //    console.log("Error", err);
+   // } else {
+      //  console.log(result);
+    //}
+//});
 
+fs.appendFileSync("./test.txt", '${Date.now()} Hey There \n')
+
+fs.cpSync("./test.txt", "./copy.txt");
+fs.unlinkSync("./copy.txt");
+console.log(fs.statSync("./test.txt"));
+fs.mkdirSync("my-docss/a/b,{recursive: true}");
+
+ 
