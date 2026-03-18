2eaceb73571c7b0cf5f68e85370aa1e7381547f6
Veena Singh <vsingh08739@gmail.com>
2026-03-18 10:45:05 +0530

    Add Index file

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const fs = request(fs);
+const url = require("url");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    return res.send("Hello From Home Page");
+});
+
+app.get('/about', (req, res) => {
+    return res.send("Hello From About Page");
+});
+
+function myHandler(req, res) {
+}
+
+const myServer = http.createServer(app);  
+    
+ 
+myServer.listen(8000, () => console.log("Server Started!"));
+
+
